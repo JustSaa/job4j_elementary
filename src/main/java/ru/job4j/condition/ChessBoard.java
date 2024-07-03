c7a6d79425b6b853fced848dd76913ffa6c872da
Salavat <Justdrumm@yandex.ru>
2024-07-03 19:56:56 +0300

    Correction by remarks

--- src/main/java/ru/job4j/condition/ChessBoard.java
+++ src/main/java/ru/job4j/condition/ChessBoard.java
@@ -3,11 +3,8 @@ package ru.job4j.condition;
 public class ChessBoard {
 
     public static int way(int x1, int y1, int x2, int y2) {
-        if (!isValid(x1) || !isValid(x2) || !isValid(y1) || !isValid(y2)) {
-            return 0;
-        }
-        int deltaX = Math.abs(x2 - x1);
-        return deltaX == Math.abs(y2 - y1) ? deltaX : 0;
+        return (!isValid(x1) || !isValid(x2) || !isValid(y1) || !isValid(y2)) ? 0
+                : (Math.abs(x2 - x1) == Math.abs(y2 - y1) ? Math.abs(x2 - x1) : 0);
     }
 
     private static boolean isValid(int coordinate) {
