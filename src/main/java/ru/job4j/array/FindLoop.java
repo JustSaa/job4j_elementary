d36afa6bbc2033e399bcff1712ab7f86d9f6b77a
Salavat <Justdrumm@yandex.ru>
2024-07-21 11:04:49 +0300

    refactor: deleted variable

--- src/main/java/ru/job4j/array/FindLoop.java
+++ src/main/java/ru/job4j/array/FindLoop.java
@@ -14,12 +14,11 @@ public class FindLoop {
     }
 
     public static int indexInRange(int[] data, int element, int start, int finish) {
-        int result = -1;
         for (int i = start; i <= finish; i++) {
             if (data[i] == element) {
                 return i;
             }
         }
-        return result;
+        return -1;
     }
 }
