5f267eb0d9a633987b1503fb3f4572ecd9ec0f31
Salavat <Justdrumm@yandex.ru>
2024-07-21 11:01:36 +0300

    refactor: deleted variables

--- src/main/java/ru/job4j/array/EndsWith.java
+++ src/main/java/ru/job4j/array/EndsWith.java
@@ -3,10 +3,8 @@ package ru.job4j.array;
 public class EndsWith {
     public static boolean endsWith(char[] word, char[] postfix) {
         boolean result = true;
-        int wordLength = word.length;
-        int postfixLength = postfix.length;
-        for (int i = 0; i < postfixLength; i++) {
-            if (word[wordLength - postfixLength + i] != postfix[i]) {
+        for (int i = 0; i < postfix.length; i++) {
+            if (word[word.length - postfix.length + i] != postfix[i]) {
                 result = false;
                 break;
             }
