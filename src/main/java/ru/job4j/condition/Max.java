793e4550485c2deda1b9e897af9578945697ef8f
Salavat <Justdrumm@yandex.ru>
2024-09-15 21:32:56 +0300

    3. Перегрузить метод max для трех чисел [#122644]

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -5,4 +5,26 @@ public class Max {
         int result = left > right ? left : right;
         return result;
     }
+
+    public static int max(short left, short right) {
+        int result = left > right ? left : right;
+        return result;
+    }
+
+    public static int max(int left, short right) {
+        int result = left > right ? left : right;
+        return result;
+    }
+
+    public static int max(int left, int right, int middle) {
+        int result = left > right ? left : right;
+        return result > middle ? result : middle;
+    }
+
+    public static int max(int a, int b, int c, int d) {
+        int result = a > b ? a : b;
+        result = result > c ? result : c;
+        result = result > d ? result : d;
+        return result;
+    }
 }
--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -31,4 +31,43 @@ class MaxTest {
         int expected = 50;
         assertThat(result).isEqualTo(expected);
     }
+
+    @Test
+    void whenMax30to50Then50() {
+        int left = 30;
+        short right = 50;
+        int result = Max.max(left, right);
+        int expected = 50;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenMax30to40Then40() {
+        short left = 30;
+        short right = 40;
+        int result = Max.max(left, right);
+        int expected = 40;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenMax50to40And30Then50() {
+        int left = 30;
+        int right = 50;
+        int middle = 40;
+        int result = Max.max(left, right, middle);
+        int expected = 50;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenMax60to30And40And50Then60() {
+        int a = 30;
+        int b = 40;
+        int c = 50;
+        int d = 60;
+        int result = Max.max(a, b, c, d);
+        int expected = 60;
+        assertThat(result).isEqualTo(expected);
+    }
 }
