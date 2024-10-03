826b3bffd1d6128e06f7be54962c3cdb137002b1
Salavat <Justdrumm@yandex.ru>
2024-10-03 19:41:37 +0300

    refactor: chages some methods

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -6,25 +6,11 @@ public class Max {
         return result;
     }
 
-    public static int max(short left, short right) {
-        int result = left > right ? left : right;
-        return result;
-    }
-
-    public static int max(int left, short right) {
-        int result = left > right ? left : right;
-        return result;
-    }
-
     public static int max(int left, int right, int middle) {
-        int result = left > right ? left : right;
-        return result > middle ? result : middle;
+        return max(left, max(middle, right));
     }
 
     public static int max(int a, int b, int c, int d) {
-        int result = a > b ? a : b;
-        result = result > c ? result : c;
-        result = result > d ? result : d;
-        return result;
+        return max(max(a, b), max(c, d));
     }
 }
